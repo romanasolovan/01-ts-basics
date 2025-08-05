e45a7f6b6f8cf54d8ac6ed5525a3be7221e38080
Romana Solovan <solo.rv95@gmail.com>
2025-08-05 17:39:35 +0300

    finalize TypeScript homework and clean up code

---
+++ src/task-6.ts
@@ -0,0 +1,8 @@
+function getFirstElement<T>(arr: T[]): T {
+    return arr[0];
+ }
+
+console.log(getFirstElement<number>([1, 2, 3]));
+console.log(getFirstElement<string>(["a", "b", "c"]));
+console.log(getFirstElement<boolean>([ true , false , true ]));
+console.log(getFirstElement<string | number | boolean>([1, "two" ,true]));
